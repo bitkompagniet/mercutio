b8ea117fa12b38c3e1504f44daa4e6bb19c0cbd5
Kristian Videmark Parkov <parkov@bitkompagniet.dk>
2016-12-14 10:59:50 +0100

    Add star roles, array matching and rationalization

--- test/role.js
+++ test/role.js
@@ -17,4 +17,40 @@ describe('Role', function() {
 	it(`admin@some is admin@some/scope`, function() {
 		(new Role('admin@some')).is(new Role('admin@some/scope')).should.be.ok;
 	});
+
+	it(`admin@users is all of ['admin@users/27', 'admin@users/36']`, function() {
+		(new Role('admin@users'))
+			.isAll([
+				new Role('admin@users/27'),
+				new Role('admin@users/36'),
+			]).should.be.ok;
+	});
+
+	it(`admin@sites is any of ['admin@sites/67', 'admin@users/36']`, function() {
+		(new Role('admin@sites'))
+			.isAny([
+				new Role('admin@sites/67'),
+				new Role('admin@users/36'),
+			]).should.be.ok;
+	});
+
+	it(`admin@sites is not all of ['admin@sites/67', 'admin@users/36']`, function() {
+		(new Role('admin@sites'))
+			.isAll(
+				new Role('admin@sites/67'),
+				new Role('admin@users/36')
+			).should.not.be.ok;
+	});
+
+	it('*@sites is member@sites/27', function() {
+		(new Role('*@sites')).is(new Role('member@sites/27')).should.be.ok;
+	});
+
+	it('viewer@sites/27 is *@sites/27', function() {
+		(new Role('viewer@sites/27')).is(new Role('*@sites/27')).should.be.ok;
+	});
+
+	it('*@sites is not member@users', function() {
+		(new Role('*@sites')).is(new Role('member@users')).should.not.be.ok;
+	});
 });
--- test/roles.js
+++ test/roles.js
@@ -1,8 +1,9 @@
-/* eslint-disable no-unused-expressions, quotes */
+/* eslint-disable no-unused-expressions, quotes, no-unused-vars */
 
 const chai = require('chai');
 const Roles = require('../lib/roles');
 const Role = require('../lib/role');
+
 const should = chai.should();
 
 const tokenRoles = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IlBhcmtvdiBEb2UiLCJhZG1pbiI6dHJ1ZSwicm9sZXMiOlsiYWRtaW5AbXkvc2NvcGUiLHsicm9sZSI6Im1lbWJlciIsInNjb3BlIjoibXkvb3RoZXIvc2NvcGUifV19.o935F74yV4ppOoKUsE7Th6R-Wkrou6zRMV-Pu3k-H10';
@@ -36,5 +37,26 @@ describe('Roles', function() {
 		roles.whereIs('editor@some/other/scope/to/be/examined').should.have.length(1);
 		roles.whereIs('editor@some').should.have.length(0);
 		roles.in('some').should.not.be.ok;
+	});
+
+	it('should be able to rationalize roles', function() {
+		const definitions = [
+			'admin@some',
+			'admin@some/other/scope',
+			'admin@users',
+			'member@sites',
+			'member@sites/27',
+		];
+
+		const roles = new Roles(definitions);
+		roles.should.have.length(5);
+
+		const rationalized = roles.rationalize();
+		rationalized.should.have.length(3);
+		rationalized.toArray().map(r => r.scope).should.eql([
+			'some',
+			'users',
+			'sites',
+		]);
 	});
 });
